1afe5f1f6df761422864e8ee7b7b9e60372b6094
Whick <62713170+WhickYT@users.noreply.github.com>
2022-09-17 22:17:06 +0200

    After 3.5 hours, after blood tears and sweat. I finally finished the snapping. And why, you might ask? Well, I hate myself and I like some challenges. Anyways, fuck you Dokkae. Suck my balls

--- Assets/_Project/Scripts/BallLauncher.cs
+++ Assets/_Project/Scripts/BallLauncher.cs
@@ -29,14 +29,14 @@ public class BallLauncher : MonoBehaviour
 
     void Update()
     {
-        _constantEndPos = camera.ScreenToWorldPoint(Input.mousePosition);
-        _direction = _constantEndPos - _start;
+        _end = camera.ScreenToWorldPoint(Input.mousePosition);
+        _direction = _end - _start;
         _angle = Vector2.Angle(Vector2.right, _direction);
         if(camera.ScreenToWorldPoint(Input.mousePosition).y < _start.y)
         {
             _angle = _angle * -1;
         }
-        Debug.Log(_angle);
+        Debug.Log(_roundTo);
         if (!camera) return;
         if (!ball) return;
         if (!canLaunch) return;
@@ -48,7 +48,6 @@ public class BallLauncher : MonoBehaviour
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            _end = camera.ScreenToWorldPoint(Input.mousePosition);
             var dir = _start - _end;
             ball.AddForce(power * dir.magnitude * dir.normalized, ForceMode2D.Impulse);
             canLaunch = false;
@@ -58,16 +57,36 @@ public class BallLauncher : MonoBehaviour
         if(_angle <= 95 && _angle >= 85)
         {
             _roundTo = 90;
-        }
-
-        if(_angle >= 175 || _angle <= -175)
+        } else if(_angle >= 175 || _angle <= -175)
         {
             _roundTo = 180;
-        }
-
-        if(_angle <= 5 && _angle >= -5)
+        } else if(_angle <= 5 && _angle >= -5)
         {
             _roundTo = 0;
+        } else if(_angle >= -95 && _angle <= -85)
+        {
+            _roundTo = -90;
+        } else
+        {
+            _roundTo = 69;
+        }
+
+
+        if(_roundTo == 90)
+        {
+            _end = new Vector2(_start.x, camera.ScreenToWorldPoint(Input.mousePosition).y);
+        } else if(_roundTo == -90)
+        {
+            _end = new Vector2(_start.x, camera.ScreenToWorldPoint(Input.mousePosition).y);
+        } else if(_roundTo == 180)
+        {
+            _end = new Vector2(camera.ScreenToWorldPoint(Input.mousePosition).x, _start.y);
+        } else if(_roundTo == 0)
+        {
+            _end = new Vector2(camera.ScreenToWorldPoint(Input.mousePosition).x, _start.y);
+        } else if(_roundTo == 69)
+        {
+            _end = camera.ScreenToWorldPoint(Input.mousePosition);
         }
 
 
@@ -75,7 +94,7 @@ public class BallLauncher : MonoBehaviour
         {
             lr.SetPosition(0, _start);
             //lr.SetPosition(1, (Vector2)camera.ScreenToWorldPoint(Input.mousePosition));
-            lr.SetPosition(1, new Vector3());
+            lr.SetPosition(1, new Vector3(_end.x, _end.y, 0));
         }
     }
 }
