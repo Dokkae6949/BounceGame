6443ffa6f4cb4c4c3595a87f975d9492ac756ad0
Whick <62713170+WhickYT@users.noreply.github.com>
2022-09-17 21:53:21 +0200

    Almost finished baby

--- Assets/_Project/Scripts/BallLauncher.cs
+++ Assets/_Project/Scripts/BallLauncher.cs
@@ -15,6 +15,11 @@ public class BallLauncher : MonoBehaviour
     
     private Vector2 _start;
     private Vector2 _end;
+    private Vector2 _constantEndPos;
+    private Vector2 _direction;
+    private float _angle;
+
+    private int _roundTo;
 
 
     private void Start()
@@ -24,6 +29,14 @@ public class BallLauncher : MonoBehaviour
 
     void Update()
     {
+        _constantEndPos = camera.ScreenToWorldPoint(Input.mousePosition);
+        _direction = _constantEndPos - _start;
+        _angle = Vector2.Angle(Vector2.right, _direction);
+        if(camera.ScreenToWorldPoint(Input.mousePosition).y < _start.y)
+        {
+            _angle = _angle * -1;
+        }
+        Debug.Log(_angle);
         if (!camera) return;
         if (!ball) return;
         if (!canLaunch) return;
@@ -42,10 +55,27 @@ public class BallLauncher : MonoBehaviour
             lr.enabled = false;
         }
 
+        if(_angle <= 95 && _angle >= 85)
+        {
+            _roundTo = 90;
+        }
+
+        if(_angle >= 175 || _angle <= -175)
+        {
+            _roundTo = 180;
+        }
+
+        if(_angle <= 5 && _angle >= -5)
+        {
+            _roundTo = 0;
+        }
+
+
         if (lr.enabled)
         {
             lr.SetPosition(0, _start);
-            lr.SetPosition(1, (Vector2)camera.ScreenToWorldPoint(Input.mousePosition));
+            //lr.SetPosition(1, (Vector2)camera.ScreenToWorldPoint(Input.mousePosition));
+            lr.SetPosition(1, new Vector3());
         }
     }
 }
